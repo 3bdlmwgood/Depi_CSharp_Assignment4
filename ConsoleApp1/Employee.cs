1d6d0e3b3e47bc146de4349f5719b295aaeac7b7
3bdlmwgood <abdullahmohamedabdelmawgood@gmail.com>
2026-09-12 17:59:24 +0300

    Add Class Employee

---
+++ ConsoleApp1/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Employee
+    {
+        public int ID {  get; set; }
+        public string Name { get; set; }
+        public string SequrityLevel {  get; set; }
+        public decimal Salary { get; set; }
+        public DateOnly HireDate { get; set; }
+        public bool Gender { get; set; }
+
+        public Employee(int Id,string Name,string SequrityLevel,decimal Salary,DateOnly HireDate,bool Gender)
+        {
+            this.ID = Id;
+            this.Name = Name;
+            this.SequrityLevel = SequrityLevel;
+            this.Salary = Salary;
+            this.HireDate = HireDate;
+            this.Gender = Gender;
+        }
+    }
+}
