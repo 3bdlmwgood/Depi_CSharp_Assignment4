1f0794e6ef5d0bfcb390cfca3aa4b27d608eeb6e
3bdlmwgood <abdullahmohamedabdelmawgood@gmail.com>
2026-09-12 18:07:20 +0300

    Add HireDate Class

---
+++ ConsoleApp1/HireDate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class HireDate
+    {
+        public int Day {  get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+
+        public HireDate()
+        {
+            this.Day = 1;
+            this.Month = 1;
+            this.Year = 1900;
+        }
+
+        public HireDate(int day,int month,int year)
+        {
+            this.Day = day;
+            this.Month = month;
+            this.Year = year;
+        }
+    }
+}
